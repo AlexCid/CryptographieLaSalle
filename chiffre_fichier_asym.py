a47d8ee450b3f4e6a4527444afff08433359db9d
Alexander Schaub <schaub.alexander@free.fr>
2023-10-13 16:56:25 +0200

    Solution partielle TD3

--- chiffre_fichier_asym.py
+++ chiffre_fichier_asym.py
@@ -2,6 +2,9 @@ import os, sys
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.primitives.asymmetric import padding as padding_asym
+from cryptography.hazmat.primitives.serialization import load_pem_public_key
+
 
 if len(sys.argv) < 4:
     exit()
@@ -21,13 +24,31 @@ padder = padding.PKCS7(128).padder()
 clair_padde = padder.update(clair) + padder.finalize()
 
 chiffre = encryptor.update(clair_padde)+encryptor.finalize()
-import pickle
+
+chiffrés_de_key = []
+# Solution de PIerre-Louis
+for destinataire in destinataires:
+    key_pub = load_pem_public_key(open(destinataire, mode="rb").read())
+    clef_chiffre = key_pub.encrypt(
+        key,
+        padding_asym.OAEP(
+            mgf=padding_asym.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+
+    chiffrés_de_key.append(clef_chiffre)
+
+
+
 
 data = {
     "iv": iv,
     "chiffre": chiffre,
-    "chiffrés_de_key" : []
+    "chiffrés_de_key" : chiffrés_de_key
 }
 
+import pickle
 pickle.dump(data, open(fichier_chiffre, mode="xb"))
 
